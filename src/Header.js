5bc146f4db843796039f7d3f6aaf57c8464593e0
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-07-01 22:28:58 -0500

    added HeaderOptions component and css file

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,16 @@
-
 import React from "react";
 import "./Header.css";
-import { Search } from "@material-ui/icons";
+import { Home, Search, SupervisorAccount } from "@material-ui/icons";
+import HeaderOptions from "./HeaderOptions";
 
 function Header() {
   return (
     <div className="header">
       <div className="header__left">
-        <img src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg" alt="" />
+        <img
+          src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg"
+          alt=""
+        />
 
         <div className="header__search">
           <Search />
@@ -15,7 +18,10 @@ function Header() {
         </div>
       </div>
 
-      <div className="header__right"></div>
+      <div className="header__right">
+        <HeaderOptions Icon={Home} title="Home" />
+        <HeaderOptions Icon={SupervisorAccount} title="My Network" />
+      </div>
     </div>
   );
 }
---
+++ src/HeaderOptions.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./HeaderOptions.css";
+
+function HeaderOptions({Icon, title}) {
+  return <div className="headerOptions">
+        {Icon && <Icon className='headerOptions__icon'/>}
+        <h3 className='headerOptions__title'>{title}</h3>
+  </div>;
+}
+
+export default HeaderOptions;
