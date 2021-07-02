3ea8cd90d2b074f211e973feb8dc2fef31f92c9c
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-07-01 22:15:13 -0500

    built out header__left and styled it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
-      
-      {/* Header */}
-      
+      <Header />
+
       {/* App Body */}
       {/* Sidebar */}
       {/* Feed */}
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,23 @@
+
 import React from "react";
-import "./Header";
+import "./Header.css";
+import { Search } from "@material-ui/icons";
 
 function Header() {
-  return <div className="header">
-      
-  </div>;
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg" alt="" />
+
+        <div className="header__search">
+          <Search />
+          <input type="text" />
+        </div>
+      </div>
+
+      <div className="header__right"></div>
+    </div>
+  );
 }
 
 export default Header;
