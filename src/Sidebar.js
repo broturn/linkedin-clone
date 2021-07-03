e2e9a9b0d8a7735bb264bbc2d35fc0d4047f438c
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-07-02 22:00:04 -0500

    built our sidebar component and styled it

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,13 +3,44 @@ import "./Sidebar.css";
 import { Avatar } from "@material-ui/core";
 
 function Sidebar() {
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sidebar__hash">#</span>
+      <p>{topic}</p>
+    </div>
+  );
+
   return (
     <div className="sidebar">
       <div className="sidebar__top">
-        <img src="" alt="" />
+        <br></br>
+        <img
+          src="https://images.unsplash.com/photo-1625119487457-08b5f1c2087c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&w=1000&q=80"
+          alt=""
+        />
         <Avatar className="sidebar__avatar" />
         <h2>Brodie Turner</h2>
         <h4>broturn@gmail.com</h4>
+      </div>
+
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber"> 2,543</p>
+        </div>
+        <div className="sidebar__stat">
+          <p>Views on post</p>
+          <p className="sidebar__statNumber"> 2,448</p>
+        </div>
+      </div>
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+        {recentItem("reactjs")}
+        {recentItem("programming")}
+        {recentItem("softwareengineering")}
+        {recentItem("design")}
+        {recentItem("developer")}
+
       </div>
     </div>
   );
