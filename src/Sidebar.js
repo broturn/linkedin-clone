0228611b422f26efe166e7e603664f9525c8a395
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-07-02 19:30:35 -0500

    started building out the sidebar component.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Sidebar.css";
+import { Avatar } from "@material-ui/core";
 
 function Sidebar() {
-  return <div className="sideBar">
-      
-  </div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <img src="" alt="" />
+        <Avatar className="sidebar__avatar" />
+        <h2>Brodie Turner</h2>
+        <h4>broturn@gmail.com</h4>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
