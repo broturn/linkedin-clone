f205d475e03752aee3d6657ba63d5b65d71d8963
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-07-02 19:22:11 -0500

    built body components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./App.css";
+import Feed from "./Feed";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
+import Widgets from "./Widgets";
 
 function App() {
   return (
@@ -8,9 +11,13 @@ function App() {
       <Header />
 
       {/* App Body */}
-      {/* Sidebar */}
-      {/* Feed */}
-      {/* Widgets */}
+      <div className="app__body">
+        <Sidebar />
+        <Feed />
+<Widgets />    
+      </div>
+     
+     
     </div>
   );
 }
